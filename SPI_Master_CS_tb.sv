d719f2841fad174c2aedd83b30a65afa87ced3d6
Vasilii Kosolapov <117596165+KosolapovVN@users.noreply.github.com>
2022-11-11 18:11:29 +0300

    Update SPI_Master_CS_tb.sv

--- SPI_Master_CS_tb.sv
+++ SPI_Master_CS_tb.sv
@@ -1,17 +1,20 @@
+// Testbench for "SPI_Master_CS"
+
 `timescale 1ns/1ns
 `include "SPI_Master.v"
+
 module SPI_Master_CS_tb();
 
 	// Parameters
-	parameter SPI_MODE = 0;
-	parameter CLKS_PER_HALF_BIT	= 4;
-	parameter MAIN_CLK_DELAY 		= 2;
-	parameter CS_INACTIVE_CLKS 	= 5;
+	parameter CS_INACTIVE_CLKS	= 5;
 	parameter MAX_BYTES_PER_CS 	= 10;
-
+	parameter CLKS_PER_HALF_BIT	= 4;
+	parameter MAIN_CLK_DELAY 	= 2;
+	parameter SPI_MODE = 0;
+	
 	// Main control
-	logic r_rst_n 		= 1'b0;
-	logic r_clk			= 1'b0;
+	logic r_rst_n 	= 1'b0;
+	logic r_clk		= 1'b0;
 	
 	// SPI Interface main ports
 	logic w_MOSI;
@@ -19,13 +22,13 @@ module SPI_Master_CS_tb();
 	logic w_CS_n;
 	
 	// User interface data load
-	logic [7:0] r_Master_TX_Byte	= 8'h00;
-	logic r_Master_TX_En				= 1'b0;
-	logic w_Master_TX_Ready;
-	logic [7:0] r_Master_RX_Byte;
-	logic r_Master_RX_EN;
 	logic [$clog2(MAX_BYTES_PER_CS+1)-1:0] w_Master_RX_Count;
 	logic [$clog2(MAX_BYTES_PER_CS+1)-1:0] r_Master_TX_Count = MAX_BYTES_PER_CS;
+	logic [7:0] r_Master_TX_Byte = 8'h00;
+	logic [7:0] r_Master_RX_Byte;
+	logic r_Master_TX_En = 1'b0;
+	logic w_Master_TX_Ready;
+	logic r_Master_RX_EN;
 	
 	// Variables
 	logic [7:0] i;
@@ -68,24 +71,23 @@ module SPI_Master_CS_tb();
 		r_Master_TX_Byte 	<= data;
 		r_Master_TX_En 	<= 1'b1;
 		@(posedge r_clk);
-		r_Master_TX_En		<= 1'b0;
+		r_Master_TX_En	<= 1'b0;
 		@(posedge w_Master_TX_Ready);
 	endtask
 
 	initial
 	begin
 		repeat(10) @(posedge r_clk);
-      r_rst_n = 1'b0;
-      repeat(10) @(posedge r_clk);
-      r_rst_n = 1'b1;
-      repeat(10) @(posedge r_clk);
+      	r_rst_n = 1'b0;
+      	repeat(10) @(posedge r_clk);
+      	r_rst_n = 1'b1;
+      	repeat(10) @(posedge r_clk);
       
 		for (i = 1; i < MAX_BYTES_PER_CS + 1; i = i + 1)
 		begin
 			SendByte(i);
 			$display("Sent out 0x%X", i, "  Received 0x%X", r_Master_RX_Byte); 
-      end  
+      	end  
 		repeat(10) @(posedge r_clk);	
 	end	
-
 endmodule
